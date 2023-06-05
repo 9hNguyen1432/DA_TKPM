dcdb5e794421299781a44ae043c37a60f69731e0
20120435 <bich1042002@gmail.com>
2023-06-05 21:35:57 +0700

    ui class page

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -5,7 +5,7 @@ class ClassPageController{
 
     async loadStudentListPage(req,res){
         let class_name = req.params.class_name;
-        res.render('class/students',{ClassName: class_name});
+        res.render('class/students',{ClassName: class_name, Teacher: "Lê Thị Ngọc Bích", StudentNumber: 100});
     }
 }
 
