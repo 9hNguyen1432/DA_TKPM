b0b2f1e31682cf6b1002d48cf9af441759280b6e
John Doe <johndoe@example.com>
2023-06-14 23:42:02 +0700

    connect database

--- config/dataConfig.js
+++ config/dataConfig.js
@@ -2,12 +2,12 @@
 var config = {
     server: "localhost",
     user: "sa",
-    password: "1",
+    password: "123",
     database: "QLHSC3",
     driver: "msnodesqlv8"
 }
 //password
 //NB: 1
 //HC: 123456
-
+//TT: 123
 module.exports = config;
