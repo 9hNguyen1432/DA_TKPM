a45eef4eb5e85b3d63153759ceadf08a3f42b9bb
NVT-Game <toainguyenprogramminggame@gmail.com>
2023-06-18 11:54:21 +0700

    do sth, do not concern

--- routers/class.router.js
+++ routers/class.router.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const classController =  require("../controllers/class.controller");
 
-router.get('/:class_name/export',classController.downloadStudentsOfClass_CSV)
 router.get('/:class_name/import',classController.importStudent);
 router.get('/:class_name/course/:course_name',classController.loadCourseDetailPage);
 router.get('/:class_name/course',classController.loadCourseListPage);
 router.get('/:class_name',classController.loadStudentListPage);
 router.get('/',classController.loadPage)
+
+router.get('/:class_name/export',classController.downloadStudentsOfClass_CSV)
+
+
+
 module.exports = router;
