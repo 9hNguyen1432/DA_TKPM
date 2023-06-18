2fe31aa8a4400cc3e48974fec482e99ee9f16526
NVT-Game <toainguyenprogramminggame@gmail.com>
2023-06-18 13:12:15 +0700

    add link to download transcript of subject

--- models/subject.model.js
+++ models/subject.model.js
@@ -12,7 +12,12 @@ module.exports = {
 
     getTranscriptOfSubject: async(idSubject, idClass) => {
         try {
-            
+            let query_string = `SELECT * FROM SUBJECT sj, EXAM ex, EXAMRESULT er, STUDENT st'
+                            + 'WHERE sj.ID = ex.SubjectID AND er.ExamID = ex.ID'
+                            + 'AND er.StudentID = st.ID'
+                            + 'AND ex.ClassID = '${idClass}' AND sj.ID = '${idSubject}'`;
+            let result = (await conn).query(query_string);
+            return (await result);
         } catch (error) {
             
         }
--- routers/class.router.js
+++ routers/class.router.js
@@ -7,7 +7,8 @@ router.get('/:class_name/import',classController.importStudent);
 router.get('/:class_name/course/:course_name',classController.loadCourseDetailPage);
 router.get('/:class_name/course',classController.loadCourseListPage);
 router.get('/:class_name',classController.loadStudentListPage);
-router.get('/',classController.loadPage)
-router.get('/:class_name/export',classController.downloadStudentsOfClass_CSV)
+router.get('/',classController.loadPage);
+router.get('/:class_name/export',classController.downloadStudentsOfClass_CSV);
+router.get('/class/:class_name/course/:course_name/download-transcript', classController.downloadTranscriptOfSubject_CSV);
 
 module.exports = router;
