f6052de381ac8478bb7dc3867a754da82a1fe431
20120442 <chinhnguyen140302@gmail.com>
2023-06-18 19:17:12 +0700

    add fake data for class, subject, paramester

--- query/qlhsc3.sql
+++ query/qlhsc3.sql
@@ -3,6 +3,21 @@ CREATE DATABASE [QLHSC3]
 --use master
 --DROP database [QLHSC3]
 
+--DROP table
+-- DROP TABLE PARAMETERS; 
+-- DROP TABLE EXAM_RESULT; 
+-- DROP TABLE RESULT; 
+-- DROP TABLE EXAM; 
+-- DROP TABLE STUDENT; 
+-- DROP TABLE CLASS_SUBJECT; 
+-- DROP TABLE SUBJECT; 
+-- DROP TABLE CLASS;
+-- DROP TABLE SEMESTER;
+-- DROP TABLE YEAR; 
+--DROP TABLE ACCOUNT;
+--DROP PROCEDURE create_account;
+
+
 USE QLHSC3
 GO
 
@@ -13,13 +28,13 @@ CREATE TABLE STUDENT (
 	dob date,
 	email varchar(100),
 	address varchar(300),
-	class_id varchar(10),
+	class_id int,
 
 	PRIMARY KEY (id),
 )
 
 CREATE TABLE CLASS (
-	id varchar(10),
+	id int IDENTITY(1,1),
 	name varchar(10),
 	amount_student int,
 	_year varchar(10),
@@ -33,8 +48,9 @@ CREATE TABLE YEAR (
 )
 
 CREATE TABLE SUBJECT (
-	id int,
-	name varchar(30),
+	id int IDENTITY(1,1),
+	name nvarchar(30),
+	_year varchar(10),
 
 	PRIMARY KEY (id),
 );
@@ -46,18 +62,23 @@ CREATE TABLE EXAM_RESULT (
 );
 
 CREATE TABLE PARAMETERS (
+	_year varchar(10),
 	min_age int,
 	max_age int,
 	max_student int,
-	num_of_class int,
-	name_class varchar(300),
+	num_of_class_10 int,
+	num_of_class_11 int,
+	num_of_class_12 int,
+	name_class_10  varchar(300),
+	name_class_11  varchar(300),
+	name_class_12  varchar(300),
 	num_of_subject int,
-	name_of_subject varchar(300),
+	name_of_subject varchar(1000),
 	standard_score float,	
 )
 
 CREATE TABLE RESULT(
-	id INT,
+	id INT IDENTITY(1,1),
 	student_id VARCHAR(10),
 	subject_id INT,
 	_semester INT,
@@ -68,7 +89,7 @@ CREATE TABLE RESULT(
 )
 
 CREATE TABLE CLASS_SUBJECT(
-	class_id VARCHAR(10),
+	class_id int,
 	subject_id INT,
 
 	PRIMARY KEY (class_id, subject_id)
@@ -83,10 +104,10 @@ CREATE TABLE SEMESTER(
 )
 
 CREATE TABLE EXAM(
-	id INT,
+	id INT IDENTITY(1,1),
 	name nvarchar(50),
 	subject_id int,
-	class_id varchar(10),
+	class_id int,
 	_semester INT,
 	_year varchar(10)
 
@@ -197,4 +218,57 @@ END
 
 EXEC create_account 'user','user'
 
-SELECT * FROM ACCOUNT
+
+-- insert fake data to dev
+
+INSERT INTO YEAR (_year) values ('2021-2022');
+INSERT INTO SEMESTER(_semester, _year) values (1, '2021-2022');
+--class
+INSERT INTO CLASS(name, amount_student, _year) values ('10A1', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('10A2', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('10A3', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('10A4', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('11A1', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('11A2', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('11A3', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('12A1', 0, '2021-2022');
+INSERT INTO CLASS(name, amount_student, _year) values ('12A2', 0, '2021-2022');
+--subject
+INSERT INTO SUBJECT(name, _year) values ('Toán', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Lý', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Hóa', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Văn', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Sử', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Địa', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Đạo Đức', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Thể dục', '2021-2022');
+INSERT INTO SUBJECT(name, _year) values ('Sinh', '2021-2022');
+
+INSERT INTO PARAMETERS (
+	_year,
+	min_age,
+	max_age,
+	max_student,
+	num_of_class_10,
+	num_of_class_11,
+	num_of_class_12,
+	name_class_10,
+	name_class_11,
+	name_class_12,
+	num_of_subject,
+	name_of_subject,
+	standard_score)
+	 values(
+	'2021-2022',
+	15,
+	20,
+	40,
+	4,
+	3,
+	2,
+	'10A1, 10A2, 10A3, 10A4',
+	'11A1, 11A2, 11A3',
+	'12A1, 12A2',
+	9,
+	'Toán, Lý, Hóa, Sinh, Văn, Sử, Địa, Công dân, Thể dục',
+	5)
