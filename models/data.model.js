11ec05ddd5d9946ee692e4e007b66220ba01e20f
20120435 <bich1042002@gmail.com>
2023-06-18 14:57:19 +0700

    create branch

--- models/data.model.js
+++ models/data.model.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     addYear: async (_id, _yearname) => {
         try {
-            var query_string = `INSERT INTO YEAR VALUES(${_id}),'${_yearname}'`;
+            var query_string = `INSERT INTO YEAR VALUES(${_id},'${_yearname}')`;
             let result = (await conn).query(query_string)
             return result;
         } catch (error) {
